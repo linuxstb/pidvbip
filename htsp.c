ef01c85a771322b76305e01b60404c655f741933
Andy Brown <andy@broadcast-tech.co.uk>
2013-03-30 17:37:13 +0000

    Auth fails even with valid users - fault with the sha1 routine somewhere

--- htsp.c
+++ htsp.c
@@ -92,24 +92,18 @@ static void transform(uint32_t state[5], const uint8_t buffer[64]){
     c = state[2];
     d = state[3];
     e = state[4];
-for(i=0; i<80; i++){
-        int t;
-        if(i<16) t= ((uint32_t*)buffer)[i];
-        else     t= rol(block[i-3]^block[i-8]^block[i-14]^block[i-16],1);
-        block[i]= t;
-        t+= e+rol(a,5);
-        if(i<40){
-            if(i<20)    t+= ((b&(c^d))^d)    +0x5A827999;
-            else        t+= ( b^c     ^d)    +0x6ED9EBA1;
-        }else{
-            if(i<60)    t+= (((b|c)&d)|(b&c))+0x8F1BBCDC;
-            else        t+= ( b^c     ^d)    +0xCA62C1D6;
-        }
-        e= d;
-        d= c;
-        c= rol(b,30);
-        b= a;
-        a= t;
+    for(i=0; i<15; i+=5){
+        R0(a,b,c,d,e,0+i); R0(e,a,b,c,d,1+i); R0(d,e,a,b,c,2+i); R0(c,d,e,a,b,3+i); R0(b,c,d,e,a,4+i);
+    }
+    R0(a,b,c,d,e,15); R1(e,a,b,c,d,16); R1(d,e,a,b,c,17); R1(c,d,e,a,b,18); R1(b,c,d,e,a,19);
+    for(i=20; i<40; i+=5){
+        R2(a,b,c,d,e,0+i); R2(e,a,b,c,d,1+i); R2(d,e,a,b,c,2+i); R2(c,d,e,a,b,3+i); R2(b,c,d,e,a,4+i);
+    }
+    for(; i<60; i+=5){
+        R3(a,b,c,d,e,0+i); R3(e,a,b,c,d,1+i); R3(d,e,a,b,c,2+i); R3(c,d,e,a,b,3+i); R3(b,c,d,e,a,4+i);
+    }
+    for(; i<80; i+=5){
+        R4(a,b,c,d,e,0+i); R4(e,a,b,c,d,1+i); R4(d,e,a,b,c,2+i); R4(c,d,e,a,b,3+i); R4(b,c,d,e,a,4+i);
     }
     state[0] += a;
     state[1] += b;
@@ -124,26 +118,16 @@ void hts_sha1_update(HTSSHA1* ctx, const uint8_t* data, unsigned int len){
 
     j = ctx->count & 63;
     ctx->count += len;
-//#if CONFIG_SMALL
-    for( i = 0; i < len; i++ ){
-        ctx->buffer[ j++ ] = data[i];
-        if( 64 == j ){
-            transform(ctx->state, ctx->buffer);
-            j = 0;
+    if ((j + len) > 63) {
+        memcpy(&ctx->buffer[j], data, (i = 64-j));
+        transform(ctx->state, ctx->buffer);
+        for ( ; i + 63 < len; i += 64) {
+            transform(ctx->state, &data[i]);
         }
+        j=0;
     }
-//#else
-//    if ((j + len) > 63) {
-//        memcpy(&ctx->buffer[j], data, (i = 64-j));
-//        transform(ctx->state, ctx->buffer);
-//        for ( ; i + 63 < len; i += 64) {
-//            transform(ctx->state, &data[i]);
-//        }
-//        j=0;
-//    }
-//    else i = 0;
-//    memcpy(&ctx->buffer[j], &data[i], len - i);
-//#endif
+    else i = 0;
+    memcpy(&ctx->buffer[j], &data[i], len - i);
 }
 
 void hts_sha1_final(HTSSHA1* ctx, uint8_t digest[20]){
@@ -532,7 +516,6 @@ int htsp_login(struct htsp_t* htsp, char* tvh_user, char* tvh_pass)
     htsp_get_string(&msg, "method");
     htsp_get_bin(&msg, "challenge", &chall, &chall_len);
     fprintf(stderr,"Received htsp: %d\n",res);
-
   }
 
   htsp_destroy_message(&msg);
@@ -547,7 +530,7 @@ int htsp_login(struct htsp_t* htsp, char* tvh_user, char* tvh_pass)
                            HMF_STR,"username",tvh_user,
                            HMF_BIN,"digest",20,d,
                            HMF_NULL);
-  fprintf(stderr,"Sending auth digest\n");
+  fprintf(stderr,"Sending auth digest (%d)\n",d);
   if ((res = htsp_send_message(htsp,&msg)) > 0) {
     fprintf(stderr,"Could not send message (authenticate)\n");
     return 1;
